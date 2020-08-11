ec8e470ca391b6598523908aa925282d7da8484d
xmj <mingjie@iscas.ac.cn>
2020-08-11 07:06:08 +0000

    Refine the test, include checking and printing.

--- main.c
+++ main.c
@@ -1,95 +1,193 @@
-#include<stdio.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 char *strcpy(char *d, const char *s);
 char *strncpy(char *d, const char *s, size_t n);
 void *vvaddint32(size_t n, const int *x, const int *y, int *z);
-void *memcpy(void* dest, const void* src, size_t n);
-size_t strlen(const char* str);
-void *saxpy(size_t n, const float a, const float* x, float* y);
-void *sgemm_nn(size_t n, size_t m, size_t p, const float* a, size_t lda, const float* b, size_t ldb, float* c, size_t ldc);
+void *memcpy(void *dest, const void *src, size_t n);
+size_t strlen(const char *str);
+void *saxpy(size_t n, const float a, const float *x, float *y);
+void *sgemm_nn(size_t n, size_t m, size_t p, const float *a, size_t lda,
+               const float *b, size_t ldb, float *c, size_t ldc);
 
-int main(void){
+void test_strcpy() {
+  #define N 9
+  char *src = "ABCDEFGH";
+  char dst[N] = "abcdefgh";
 
-	printf("\033[1m\033[45;33m rvv-benchmark \033[0m\n");
-        
-	//--------------------------//
-	const char srcstr[] = "ABCDEFGH";
-              char dststr[] = "abcdefgh";
-		
-        printf("//strcpy.s ----------------------// \n");
-      	strcpy(dststr, srcstr);
-	printf(" srcstr[] = ABCDEFGH, dststr[] = abcdefgh, after calling strcpy.s     \n ");
-	printf("dststr[] = ");
-	printf("%s \n", dststr);
-        printf("//strcpy.s Pass ----------------------// \n\n");	
+  printf(">>> Testing strcpy.s...\n");
+  printf(">>> src = %s, dst = %s\n", src, dst);
+  strcpy(dst, src);
+  printf(">>> after calling strcpy(dst, src):\n");
+  printf(">>> src = %s, dst = %s\n", src, dst);
 
-        //---------------------------//
-	printf("//strncpy.s---------------------// \n");
-	const char srcnstr[] = "ABCDEFGH";
-              char dstnstr[] = "abcdefgh";
-	strncpy(dstnstr, srcnstr, 3);
-	printf(" srcnstr[] = ABCDEFGH, dstnstr[] = abcdefgh, n=3,  after calling strcpy.s     \n ");
-        printf("dstnstr[] = ");
-	printf("%s \n", dstnstr);
-        printf("//strncpy.s Pass ----------------------// \n\n");
+  /* Check result. */
+  bool status = true;
+  for (int i = 0; i < N; ++i) {
+    if (dst[i] != src[i]) {
+      status = false;
+      break;
+    }
+  }
+  printf(">>> %s\n\n", status ? "Pass!" : "Failed!");
+  #undef N
+}
 
-        //---------------------------//
-       	printf("//vvaddint32.s---------------------// \n");
-	size_t n=5;
-        const int x[5]= { 1, 2, 3, 4, 5};
-	const int y[5]= { 1, 1, 1, 1, 1};
-              int z[5];
-        vvaddint32(n, x, y, z);
-	for(int i=0; i<n; i++)
-	printf("%d, ", z[i]);
-	printf("\n");
-        printf("//vvaddint32.s Pass---------------------// \n\n");
+void test_strncpy() {
+  #define N 3
+  char *src = "ABCDEFGH";
+  char dst[] = "abcdefgh";
 
-        //---------------------------//
-       	printf("//memcpy.s and strlen.s---------// \n");
-        char* src="hellowilliam";
-	char dest[20];
-	size_t m= strlen(src)+1;
-	memcpy(dest, src, m);
-	printf("%s \n", dest);
-        printf("//memcpy.s and strlen.s Pass---------// \n\n");
+  printf(">>> Testing strncpy.s...\n");
+  printf(">>> src = %s, dst = %s\n", src, dst);
+  strncpy(dst, src, N);
+  printf(">>> after calling strncpy(dst, src, %d):\n", N);
+  printf(">>> src = %s, dst = %s\n", src, dst);
 
-        //---------------------------//
-       	printf("//saxpy.s---------------------// \n");
-        const float xx[7] = {7, 7, 7, 7, 7, 7, 7};
-              float yy[7] = {1, 1, 1, 1, 1, 1, 1};
-	const float a=2;
-	size_t p=7;
-	saxpy(p, a, xx, yy);
-	for(int i=0; i<p; i++)
-        printf("%f, ", yy[i]);
-        printf("\n");
-        printf("//saxpy.s Pass---------------------// \n\n");
-       
-        //---------------------------//
-	printf("//sgemm.S---------------------// \n");
-        unsigned int n1 = 16;
-        unsigned int m1 = 16;
-        unsigned int k1 = 2;
+  /* Check result. */
+  bool status = true;
+  for (int i = 0; i < N; ++i) {
+    if (dst[i] != src[i]) {
+      status = false;
+      break;
+    }
+  }
+  printf(">>> %s\n\n", status ? "Pass!" : "Failed!");
+  #undef N
+}
 
-        const float a1[16][2] = {0.7, 1.6, 0.1, 0.2, 1.1, 1.2, 1.1, 0.0, 
-		                0.3, 0.1, 0.1, 0.7, 0.2, 0.2, 0.5, 2.1,
-			       	0.1, 0.1, 1.0, 0.0, 0.0, 0.8, 0.2, 1.3,
-			       	0.7, 1.6, 0.6, 0.8, 1.2, 1.5, 1.4, 0.3,};
+void test_vvaddint32() {
+  #define N 5
+  const int x[N]= { 1, 2, 3, 4, 5};
+  const int y[N]= { 1, 1, 1, 1, 1};
+  int z[N];
 
-        const float b1[2][16] = {1.7, 0.1, 0.3, 0.7, 0.3, 0.5, 0.9, 1.1,
-                                2.2, 0.6, 1.7, 0.4, 1.1, 1.4, 1.2, 1.8,
-                                2.1, 1.4, 0.9, 1.2, 0.8, 0.6, 0.4, 1.2,
-                                0.7, 0.2, 0.7, 0.8, 0.1, 1.0, 0.2, 0.1};
+  printf(">>> Testing vvaddint32.s...\n");
+  printf(">>> x = ");
+  for (int i = 0; i < N; i++)
+    printf("%d, ", x[i]);
+  printf("\n");
 
-	float c1[16][16] = { 0 };
- 
-        size_t lda = 1;
-        size_t ldb = 1;
-        size_t ldc = 1;
+  printf(">>> y = ");
+  for (int i = 0; i < N; i++)
+    printf("%d, ", y[i]);
+  printf("\n");
 
-        sgemm_nn(n1, m1, k1, &a1[0][0], lda, &b1[0][0], ldb, &c1[0][0], ldc);
-        printf("//sgemm.S Pass---------------------// \n\n");
+  vvaddint32(N, x, y, z);
 
-	return 0;
+  printf(">>> after calling vvaddint32(%d, x, y, z):\n", N);
+  printf(">>> z = ");
+  for (int i = 0; i < N; i++)
+    printf("%d, ", z[i]);
+  printf("\n");
+
+  /* Check result. */
+  bool status = true;
+  for (int i = 0; i < N; ++i) {
+    if (z[i] != x[i] + y[i]) {
+      status = false;
+      break;
+    }
+  }
+  printf(">>> %s\n\n", status ? "Pass!" : "Failed!");
+  #undef N
+}
+
+void test_memcpy_strlen() {
+  char *src = "Hello World!";
+  char dst[20];
+  size_t len = strlen(src) + 1;
+
+  printf(">>> Testing memcpy.s and strlen.s...\n");
+  printf(">>> src = %s\n", src);
+  printf(">>> after calling memcpy(dst, src, %zu)\n", len);
+  memcpy(dst, src, len);
+  printf(">>> dst = %s\n", dst);
+
+  /* Check result. */
+  bool status = true;
+  for (int i = 0; i < len; ++i) {
+    if (dst[i] != src[i]) {
+      status = false;
+      break;
+    }
+  }
+  printf(">>> %s\n\n", status ? "Pass!" : "Failed!");
+}
+
+void test_saxpy() {
+  #define N 7
+  const float x[N] = {7, 7, 7, 7, 7, 7, 7};
+        float y[N] = {1, 1, 1, 1, 1, 1, 1};
+  const float z[N] = {15, 15, 15, 15, 15, 15, 15};
+  const float a = 2;
+
+  printf(">>> Testing saxpy.s...\n");
+  printf(">>> x = ");
+  for (int i = 0; i < N; i++)
+    printf("%f, ", x[i]);
+  printf("\n");
+
+  printf(">>> y = ");
+  for (int i = 0; i < N; i++)
+    printf("%f, ", y[i]);
+  printf("\n");
+
+  printf(">>> a = %f\n", a);
+
+  printf(">>> after calling saxpy(%d, a, x, y)\n", N);
+  saxpy(N, a, x, y);
+
+  printf(">>> y = ");
+  for (int i = 0; i < N; i++)
+    printf("%f, ", y[i]);
+  printf("\n");
+
+  /* Check result. */
+  bool status = true;
+  for (int i = 0; i < N; ++i) {
+    if (y[i] != z[i]) {
+      status = false;
+      break;
+    }
+  }
+  printf(">>> %s\n\n", status ? "Pass!" : "Failed!");
+  #undef N
+}
+
+void test_sgemm_nn() {
+  #define N 16
+  #define K 2
+  const float a[N][K] = {
+    0.7, 1.6, 0.1, 0.2, 1.1, 1.2, 1.1, 0.0,
+    0.3, 0.1, 0.1, 0.7, 0.2, 0.2, 0.5, 2.1,
+    0.1, 0.1, 1.0, 0.0, 0.0, 0.8, 0.2, 1.3,
+    0.7, 1.6, 0.6, 0.8, 1.2, 1.5, 1.4, 0.3
+  };
+  const float b[K][N] = {
+    1.7, 0.1, 0.3, 0.7, 0.3, 0.5, 0.9, 1.1,
+    2.2, 0.6, 1.7, 0.4, 1.1, 1.4, 1.2, 1.8,
+    2.1, 1.4, 0.9, 1.2, 0.8, 0.6, 0.4, 1.2,
+    0.7, 0.2, 0.7, 0.8, 0.1, 1.0, 0.2, 0.1
+  };
+  float c[N][N] = {0};
+
+  printf(">>> Testing sgemm.S...\n");
+  sgemm_nn(N, N, K, (float*)a, 1, (float*)b, 1, (float*)c, 1);
+
+  /* TODO: Check result. */
+  bool status = true;
+  printf(">>> %s\n\n", status ? "Pass!" : "Failed!");
+  #undef N
+}
+
+int main(void)
+{
+  printf("\nRunning rvv-benchmark...\n");
+  test_strcpy();
+  test_strncpy();
+  test_vvaddint32();
+  test_memcpy_strlen();
+  test_saxpy();
+  test_sgemm_nn();
 }
